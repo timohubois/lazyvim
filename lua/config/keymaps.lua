2353d17db7f1adced337f48b6e94860082f075ae
Timo Hubois <info@pixelsaft.de>
2025-04-05 15:52:50 +0200

    feat(keymaps): enhance window resizing and line movement for macOS with German layout

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -4,3 +4,16 @@
 
 -- Add a keymap to exit terminal mode in LazyVim
 vim.keymap.set("t", "<esc><esc>", "<cmd>stopinsert<cr>", { desc = "Exit Terminal Mode" })
+
+-- Fix window resizing on macOS when mission control is enabled.
+vim.keymap.set("n", "<M-C-Up>", "<cmd>resize +2<cr>", { desc = "Increase Window Height" })
+vim.keymap.set("n", "<M-C-Down>", "<cmd>resize -2<cr>", { desc = "Decrease Window Height" })
+
+-- Move Lines in a non conflicting way with special characters on macos with german keyboard layout,
+-- due to lazyvim default hjkl is conflicting with the german keyboard layout.
+vim.keymap.set("n", "<A-Down>", "<cmd>execute 'move .+' . v:count1<cr>==", { desc = "Move Down" })
+vim.keymap.set("n", "<A-Up>", "<cmd>execute 'move .-' . (v:count1 + 1)<cr>==", { desc = "Move Up" })
+vim.keymap.set("i", "<A-Down>", "<esc><cmd>m .+1<cr>==gi", { desc = "Move Down" })
+vim.keymap.set("i", "<A-Up>", "<esc><cmd>m .-2<cr>==gi", { desc = "Move Up" })
+vim.keymap.set("v", "<A-Down>", ":<C-u>execute \"'<,'>move '>+\" . v:count1<cr>gv=gv", { desc = "Move Down" })
+vim.keymap.set("v", "<A-Up>", ":<C-u>execute \"'<,'>move '<-\" . (v:count1 + 1)<cr>gv=gv", { desc = "Move Up" })
