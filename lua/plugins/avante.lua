d3607c59ad26270d51dec23b45002424cd128bf9
Timo Hubois <info@pixelsaft.de>
2025-04-05 15:50:21 +0200

    feat(avante): exclude for now

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,21 +1,48 @@
--- avante.lua
+-- Exclude for now.
+-- stylua: ignore
+if true then return {} end
+
+--[[
+  Avante.nvim configuration for Neovim
+
+  See: https://github.com/yetone/avante.nvim
+--]]
+
 return {
     {
         "yetone/avante.nvim",
         event = "VeryLazy",
-        lazy = true,
-        version = false, -- set this if you want to always pull the latest change
+        version = false, -- Never set this value to "*"! Never!
         opts = {
-          provider = "copilot",
+            provider = "copilot",
         },
-        auto_suggestions_provider = "copilot",
         build = "make",
         dependencies = {
-          "nvim-treesitter/nvim-treesitter",
-          "stevearc/dressing.nvim",
-          "nvim-lua/plenary.nvim",
-          "MunifTanjim/nui.nvim",
-          "zbirenbaum/copilot.lua"
+            "nvim-treesitter/nvim-treesitter",
+            "nvim-lua/plenary.nvim",
+            "MunifTanjim/nui.nvim",
+            "zbirenbaum/copilot.lua",
+            {
+                "HakonHarnes/img-clip.nvim",
+                event = "VeryLazy",
+                opts = {
+                    default = {
+                        embed_image_as_base64 = false,
+                        prompt_for_file_name = false,
+                        drag_and_drop = {
+                            insert_mode = true,
+                        },
+                        use_absolute_path = true,
+                    },
+                },
+            },
+            {
+                'MeanderingProgrammer/render-markdown.nvim',
+                opts = {
+                    file_types = { "markdown", "Avante" },
+                },
+                ft = { "markdown", "Avante" },
+            },
         },
     }
 }
